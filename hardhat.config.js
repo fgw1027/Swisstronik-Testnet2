9d8384b510059f30fd4dc5ffb9825a8c066c4eb9
fgw1027 <fgw1027@gmail.com>
2024-08-04 21:23:54 +0800

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,6 @@
 require("@nomicfoundation/hardhat-toolbox");
 
-const PRIVATE_KEY = "0bf0e4eee53704e026b1dc9992a017442bdce2a9fd70664c2d5c5b9a7943ed48";
+const PRIVATE_KEY = "";
 
 module.exports = {
   defaultNetwork: "swisstronik",
